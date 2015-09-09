f00c30e673c1b4ed7145485522b9d596f540ea3a
Lilac <lilacoren@gmail.com>
2015-09-09 11:46:38 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'application#hello'
+  root to: 'application#hello'
 
   # See how all your routes lay out with "rake routes"
 
